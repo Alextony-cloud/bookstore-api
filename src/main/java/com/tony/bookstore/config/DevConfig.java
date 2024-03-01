53517d62532ba81e662e5f4169f00a8192ae357c
Alextony-cloud <alextonyrodrigues@gmail.com>
2024-03-01 16:50:44 -0300

    criação do perfil de dev

--- src/main/java/com/tony/bookstore/config/DevConfig.java
+++ src/main/java/com/tony/bookstore/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.tony.bookstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,17 +9,22 @@ import org.springframework.context.annotation.Profile;
 import com.tony.bookstore.service.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String Strategy;
+	
 	@Bean
-	public DBService instanciaBaseDados() {
+	public boolean instanciaBaseDados() {
 		
-		dbService.instanciaBaseDados();
-		return dbService;
+		if(Strategy.equals("create")) {
+			 this.dbService.instanciaBaseDados();
+		}
+		return false;
 		
 		
 	}
