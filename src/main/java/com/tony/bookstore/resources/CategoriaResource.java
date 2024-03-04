623bf7a5e3e24ee8f648733581efedefe02973b0
Alextony-cloud <alextonyrodrigues@gmail.com>
2024-03-04 18:49:06 -0300

    POST para categoria

--- src/main/java/com/tony/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/tony/bookstore/resources/CategoriaResource.java
@@ -1,14 +1,19 @@
 package com.tony.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tony.bookstore.domain.Categoria;
 import com.tony.bookstore.dtos.CategoriaDTO;
@@ -32,5 +37,12 @@ public class CategoriaResource {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria>create(@RequestBody Categoria obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
